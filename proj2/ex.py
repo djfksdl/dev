dbf8004774afdb9e9a73f45f4501803970c065a5
djfksdl <djfksdl@naver.com>
2024-07-05 17:56:28 +0900

    ai기초

--- proj2/ex.py
+++ proj2/ex.py
@@ -1,4 +1,5 @@
 # example > person_detection > demo_analysis.py 붙여서 setp정리하기
+# pip install onnxruntime 이후 실행하면 오류남 -> 터미널에서 오류 난줄에 84line어쩌고가 있음. 그거 컨트롤로 클릭해서 들어가서 init뒤에 _ 붙여주기 *2 -> 다시 실행하면 jpg들어가면 네모 생겨있음.
 
 # STEP 1
 import cv2
@@ -6,7 +7,7 @@ import numpy as np
 import insightface
 from insightface.app import FaceAnalysis
 
-# STEP 2
+# STEP 2 -> 어디에 다운받아지냐면 컨트롤로 들어가면 있음
 app = FaceAnalysis()
 app.prepare(ctx_id=0, det_size=(640,640))
 
@@ -28,3 +29,5 @@ for face in faces:
 feats = np.array(feats, dtype=np.float32)
 sims = np.dot(feats, feats.T)
 print(sims)
+
+# tlf
