9563d20dcf517d343d6ef91de683df7a623a65f0
djfksdl <djfksdl@naver.com>
2024-07-05 17:49:43 +0900

    .

--- proj2/ex.py
+++ proj2/ex.py
@@ -1,28 +1,26 @@
-import argparse
+# example > person_detection > demo_analysis.py 붙여서 setp정리하기
+
+# STEP 1
 import cv2
-import sys
 import numpy as np
 import insightface
 from insightface.app import FaceAnalysis
-from insightface.data import get_image as ins_get_image
 
-assert insightface.__version__>='0.3'
-
-parser = argparse.ArgumentParser(description='insightface app test')
-# general
-parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
-parser.add_argument('--det-size', default=640, type=int, help='detection size')
-args = parser.parse_args()
-
+# STEP 2
 app = FaceAnalysis()
-app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
+app.prepare(ctx_id=0, det_size=(640,640))
 
+# STEP 3
+from insightface.data import get_image as ins_get_image
 img = ins_get_image('t1')
+
+# STEP 4
 faces = app.get(img)
 assert len(faces)==6
+
+# STEP 5
 rimg = app.draw_on(img, faces)
 cv2.imwrite("./t1_output.jpg", rimg)
-
 # then print all-to-all face similarity
 feats = []
 for face in faces:
